1951a83e58b7370832078fe516079e52d13ff4ea
Qi Wu <wilson.over.cloud@gmail.com>
2021-12-18 18:38:51 +0800

    move neuralcache into a different project

--- samples/neuralcache/util-cuda/helper_cuda_texture.h
+++
@@ -1,9 +0,0 @@
-#ifndef HELPER_CUDA_TEXTURE_H
-#define HELPER_CUDA_TEXTURE_H
-
-#include <cuda_runtime.h>
-
-cudaTextureObject_t create_mipmap_rgba32f_texture(void* data, int width, int height);
-cudaTextureObject_t create_pitch2d_rgba32f_texture(void* data, int width, int height);
-
-#endif // HELPER_CUDA_TEXTURE_H
